e856b331f644f8c8ba29e285b194fc070d92ddb9
VitorHmaia <vitorhmaia@outlook.com>
2023-11-23 20:32:19 -0300

    chore: migration db to elephantSql

--- core/settings.py
+++ core/settings.py
@@ -76,9 +76,13 @@ WSGI_APPLICATION = 'core.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "auvohyjc",
+        "USER": "auvohyjc",
+        "PASSWORD": "xnpek4I4cqT5Eqb53j2aZcjffzxk3bm-",
+        "HOST": "isabelle.db.elephantsql.com",
+        "PORT": "5432",
     }
 }
 
@@ -105,7 +109,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'pt-br'
 
 TIME_ZONE = 'UTC'
 
