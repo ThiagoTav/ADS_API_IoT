7c8865721b50d0e41413c4e64156c1c8ae098c1c
Gabriel Ramos <gramos.sk9@gmail.com>
2023-11-23 21:35:19 -0300

    feat:creating the box component creation of the box component to place the contents inside the box

--- iot/src/App.js
+++ iot/src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import Caixa from './components/caixa';
 
 function App() {
   return (
@@ -17,7 +18,9 @@ function App() {
         >
           Learn React
         </a>
+        <Caixa/>
       </header>
+     
     </div>
   );
 }
