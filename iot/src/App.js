4b219c627c3b794732469b7ea4d60fb687b7ba01
Gabriel Ramos <gramos.sk9@gmail.com>
2023-11-24 18:54:33 -0300

    style:removal of unnecessary styles removed some non-essential components for the screen

--- iot/src/App.js
+++ iot/src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import './App.css';
 import Caixa from './components/caixa';
 import Circulo from './components/circulo/circulo';
@@ -7,18 +6,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      
         <Caixa/>
         <Circulo/>
         <Circulo/>
