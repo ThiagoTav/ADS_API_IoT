e2761e5d855ecb2883652dc4ca0057e4063466a9
VitorHmaia <vitorhmaia@outlook.com>
2023-12-04 19:53:58 -0300

    styles: modernizing the user card with dark colors

--- Iot-native/components/Profile/ProfileCard.tsx
+++ Iot-native/components/Profile/ProfileCard.tsx
@@ -1,19 +1,19 @@
 import React from 'react';
 import { StyleSheet, View, Text, Image } from 'react-native';
 
-export function ProfileCard ({nome, cidade}) {
+export function ProfileCard({ nome, cidade }) {
   return (
     <View style={styles.container}>
-    <View style={styles.rectangle}>
-      <View style={styles.circle}>
-        <Image source={require('../../assets/images/Profile.png')} style={styles.centerImg} />
+      <View style={styles.rectangle}>
+        <View style={styles.circle}>
+          <Image source={require('../../assets/images/Profile.png')} style={styles.centerImg} />
         </View>
-            <View style={styles.containerTxt}>
-                <Text style={styles.TextProfile}>Nome:{nome}</Text>
-                <Text style={styles.TextProfileCity}>Cidade:{cidade}</Text>
-            </View>
+        <View style={styles.containerTxt}>
+          <Text style={styles.textProfile}>Nome: {nome}</Text>
+          <Text style={styles.textProfileCity}>Cidade: {cidade}</Text>
         </View>
       </View>
+    </View>
   );
 }
 
@@ -25,55 +25,46 @@ const styles = StyleSheet.create({
     width: 271,
     borderRadius: 10,
     justifyContent: 'center',
-    gap: 50,
+    marginVertical: 10,
   },
 
   circle: {
-    borderWidth: 1,
-    borderRadius: 50, // Use metade da largura/altura para criar um círculo
+    borderRadius: 50,
     padding: 10,
-    backgroundColor: '#7ED7C1',
-    width: 65, // Ajuste conforme necessário
-    height: 65, // Ajuste conforme necessário
-    justifyContent: 'space-between',
-    flexDirection: 'row',
-    alignItems: 'center', // Centraliza verticalmente a imagem e o texto
-    alignSelf: 'center',
-    },
+    backgroundColor: '#2C3E50', // Cor de fundo escuro
+    width: 65,
+    height: 65,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
 
   centerImg: {
-    fontSize: 16, // Ajuste conforme necessário
-    fontWeight: 'bold',
-    color: 'black',
     width: 50,
     height: 50,
-    right: 3.5,
   },
 
   rectangle: {
-    borderWidth: 1, // Adiciona uma borda ao redor do retângulo
     borderRadius: 10,
-    padding: 10, // Espaçamento interno para o texto dentro do retângulo
-    backgroundColor: '#7ED7C1',
+    padding: 10,
+    backgroundColor: '#34495E', // Cor de fundo escuro
     width: 360,
     height: 120,
-    justifyContent: 'space-between',
     flexDirection: 'row',
+    alignItems: 'center',
   },
 
   containerTxt: {
-    justifyContent: 'center',
-    alignItems: 'flex-start',
-    right: 120,
-    fontSize: 20,  
+    marginLeft: 10,
   },
 
-  TextProfile: {
-    
+  textProfile: {
+    fontSize: 16,
+    fontWeight: 'bold',
+    color: '#ECF0F1', // Cor do texto branco
   },
 
-  TextProfileCity: {
-   
-  }
-
+  textProfileCity: {
+    fontSize: 14,
+    color: '#BDC3C7', // Cor do texto cinza claro
+  },
 });
