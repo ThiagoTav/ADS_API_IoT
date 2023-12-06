afd68f2aae33f8627cb38e48ba59e4e6b78ce5d1
thiagotavares2002@gmail.com <thiagotavares2002@gmail.com>
2023-12-06 18:29:45 -0300

    front funcionando, so falta arrumar o "axios"

--- core/settings.py
+++ core/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-yq%f8e^-c!l%$q=gpn!2snjj^a9ars$ok@y_2fltfza$j8czno
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'projetoIoT',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
@@ -77,6 +78,7 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'core.wsgi.application'
 
+ASGI_APPLICATION = 'projetoIoT.routing.application' 
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
