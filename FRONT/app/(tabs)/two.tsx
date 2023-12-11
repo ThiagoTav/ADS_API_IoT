adbee7ae06e312da65cfee7754dd91644b6c9a36
Augustoo22 <victoraugustodocarmo32@gmail.com>
2023-12-11 18:43:08 -0300

    feat: Implementado grafico

--- FRONT/app/(tabs)/two.tsx
+++ FRONT/app/(tabs)/two.tsx
@@ -1,107 +1,11 @@
 import React from 'react';
-import { LineChart } from 'react-native-chart-kit';
 import { View, StyleSheet } from 'react-native';
-
-const GraphicComponent = ({ data }) => {
-  const getYValues = data.map(point => point.Y);
-  const getXValues = data.map(point => point.X);
-  const labels = data.map(point => point.horario);
-
-  return (
-    <View style={styles.chartContainer}>
-      {/* Gráfico para o eixo Y */}
-      <LineChart
-        data={{
-          labels: labels,
-          datasets: [
-            {
-              data: getYValues,
-            },
-          ],
-        }}
-        width={350}
-        height={200}
-        yAxisLabel={'Y '}
-        xAxisLabel={'Horário'} // Adicionando o rótulo do eixo X
-        chartConfig={{
-          backgroundGradientFrom: '#1A2533',
-          backgroundGradientTo: '#1A2533',
-          decimalPlaces: 2,
-          color: (opacity = 1) => `rgba(255, 215, 0, ${opacity})`,
-          labelColor: (opacity = 1) => `rgba(236, 240, 241, ${opacity})`,
-          style: {
-            borderRadius: 10,
-          },
-        }}
-        bezier
-        style={{
-          marginVertical: 8,
-          borderRadius: 10,
-        }}
-      />
-
-      {/* Gráfico para o eixo X */}
-      <LineChart
-        data={{
-          labels: labels,
-          datasets: [
-            {
-              data: getXValues,
-            },
-          ],
-        }}
-        width={350}
-        height={200}
-        yAxisLabel={'X '}
-        xAxisLabel={'Horário'} // Adicionando o rótulo do eixo X
-        chartConfig={{
-          backgroundGradientFrom: '#1A2533',
-          backgroundGradientTo: '#1A2533',
-          decimalPlaces: 2,
-          color: (opacity = 1) => `rgba(255, 215, 0, ${opacity})`,
-          labelColor: (opacity = 1) => `rgba(236, 240, 241, ${opacity})`,
-          style: {
-            borderRadius: 10,
-          },
-        }}
-        bezier
-        style={{
-          marginVertical: 8,
-          borderRadius: 10,
-        }}
-      />
-    </View>
-  );
-};
-
-const styles = StyleSheet.create({
-  chartContainer: {
-    alignItems: 'center',
-    justifyContent: 'center',
-    backgroundColor: '#1A2533',
-    paddingVertical: 20,
-    borderRadius: 20,
-    marginTop: 20,
-  },
-});
-
-export default GraphicComponent;
-
-
-
-
-
-
-
-
-import React from 'react';
-import { View, StyleSheet } from 'react-native';
-import GraphicComponent from '../../components/Graphic/Graphic';
+import GraphicComponent from '../../components/Graphic/GraphicComponent';
 
 export default function TabTwoScreen() {
   // Supondo que você tenha algum conjunto de dados para passar para o gráfico
   const dadosDoGrafico = [
-    { Y: 22, X: 40, horario: '10:00' },
+    { Y: 22, X: 43, horario: '10:00' },
     { Y: 29, X: 45, horario: '11:00 ' },
     { Y: 20, X: 4, horario: '12:00 ' },
     // Adicione mais pontos conforme necessário
@@ -119,7 +23,5 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
-    backgroundColor: '#1A2533', 
-
   },
 });
